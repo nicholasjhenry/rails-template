71cc322636711226379d0fd4ca32c88798c17f5c
Nicholas Henry <nicholas.henry@gmail.com>
2010-09-18 14:46:14 -0400

    Fixes some formatting issues

--- patterns/app/controllers/application_controller.rb
+++ patterns/app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  include ExceptionNotification::Notifiable
+
   helper :all
   protect_from_forgery
-  include ExceptionNotification::Notifiable
 end
