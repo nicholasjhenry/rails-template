a17db08956552944c681ef6bf6be9f613a67c348
Nicholas Henry <nicholas.henry@gmail.com>
2011-03-05 18:21:30 -0500

    Remove rspec gem, rspec-rails will require it as a dependency

--- recipes/rspec.rb
+++ recipes/rspec.rb
@@ -1,5 +1,4 @@
 gem 'rspec-rails', '~>2.4', :group => [:test, :development, :cucumber]
-gem 'rspec', '~>2.4', :group => :test
 gem 'shoulda', '~>2.11', :group => :test
 
 post_bundler_strategies << lambda do
