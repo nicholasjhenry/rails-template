01800f48bf6b78a2948e3ac276860b365c364a46
Nicholas Henry <nicholas.henry@gmail.com>
2011-06-27 13:14:14 -0400

    Update Compass Gem

--- recipes/compass.rb
+++ recipes/compass.rb
@@ -1,4 +1,4 @@
-gem 'compass', '~>0.10'
+gem 'compass', '~>0.11'
 
 post_bundler_strategies << lambda do
   run 'compass init rails --using blueprint/semantic --css-dir=public/stylesheets/compiled --sass-dir=app/stylesheets --syntax sass'
