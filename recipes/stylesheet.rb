1f6b0b7b1b59e4e1f91650fe719d8f7c842014a5
Nicholas Henry <nicholas.henry@gmail.com>
2011-08-04 17:00:05 -0400

    Add notice about compass on heroku

--- recipes/stylesheet.rb
+++ recipes/stylesheet.rb
@@ -3,3 +3,11 @@ gem 'compass', '~>0.11'
 post_bundler_strategies << lambda do
   run 'compass init rails --using blueprint/semantic --css-dir=public/stylesheets/compiled --sass-dir=app/stylesheets --syntax sass'
 end
+
+message = <<-END
+If you are deploy Heroku, add tmp to the css_dir in config/compass.rb:
+
+css_dir = "tmp/public/stylesheets/compiled"
+END
+
+success_notice << message
