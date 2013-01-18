6d7549736aadc0f12fb1b8b285b7df8e0b777007
Nicholas Henry <nicholas.henry@gmail.com>
2013-01-18 16:21:46 -0500

    Remove unused dev gems

--- recipes/development.rb
+++ recipes/development.rb
@@ -1,6 +1,2 @@
 gem 'awesome_print', :group => :development
-gem 'axe', :group => :development
-gem 'hirb', :group => :development
 gem 'interactive_editor', :group => :development
-gem 'ruby-debug19', :require => 'ruby-debug', :group => :development
-gem 'slowgrowl', :group => :development
