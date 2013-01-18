bb148234009d61f750759658218286cf5ccc4fa3
Nicholas Henry <nicholas.henry@gmail.com>
2013-01-18 16:31:46 -0500

    Really simplify unit testing setup

--- recipes/unit_testing.rb
+++ recipes/unit_testing.rb
@@ -1,56 +1,5 @@
-gem 'autotest-rails'
-
-gem 'faker'
-
-gem 'shoulda', '~>2.11', :group => :test
-
-gem 'factory_girl_rails', :group => [:test, :cucumber]
-
-gem 'rspec-rails', '~>2.5', :group => [:test, :development, :cucumber]
+gem 'rspec-rails', :group => :test
 
 post_bundler_strategies << lambda do
   generate 'rspec:install'
-end
-
-gem 'fuubar'
-
-success_notice << <<-END
-Fuubar: add the following to .rspec
-
---format Fuubar
-END
-
-gem 'bourne', '~>1.0', :group => :test
-
-success_notice << <<-END
-Bourne: add the following to spec/spec_helper
-
-config.mock_with :mocha
-END
-
-gem 'cover_me', '~>1.1.2', :group => :test
-
-gem 'email_spec', '~>1.2', :group => [:test, :cucumber]
-
-post_bundler_strategies << lambda do
-  generate 'email_spec:steps'
-
-  # configure email_sepc for Cucumber
-  insert_into_file "features/support/env.rb", :after => "require 'cucumber/rails/world'\n" do
-    <<-RUBY
-    require 'email_spec'
-    require 'email_spec/cucumber'
-    RUBY
-  end
-
-  # configure email_spec for Rspec
-  insert_into_file "spec/spec_helper.rb", "require 'email_spec'", :after => "require 'rspec/rails'\n"
-
-  insert_into_file "spec/spec_helper.rb", :before => "end" do
-    <<-RUBY
-
-    config.include(EmailSpec::Helpers)
-    config.include(EmailSpec::Matchers)
-    RUBY
-  end
 end
